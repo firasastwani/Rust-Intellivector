3d168bc1bd21d705618ab9c31110677d28a58b1d
firasastwani <firasastwani1@gmail.com>
2026-03-02 20:36:44 -0500

    cosine similarity impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use crate::similarity::cosine_similarity;
+
+mod similarity;
+
 fn main() {
-    println!("Hello, world!");
+    let v1 = vec![1.0, 2.0, 3.0, 4.0, 5.0];
+    let v2 = vec![-1.0, -2.0, -3.0, -4.0, -5.0];
+
+    let res = cosine_similarity(&v1, &v2);
+
+    println!("{res}");
 }
